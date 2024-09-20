b021d1c7860533bbe09dac184e7f5b9abf4aff75
rpallin0705 <rpalomarelinares@gmail.com>
2024-09-20 10:17:00 +0100

    Cambios

--- ACDT/BorrarMascotas.java
+++ ACDT/BorrarMascotas.java
@@ -4,12 +4,14 @@ public class BorrarMascotas {
     public static void main(String[] args) {
         File fileToDelete = new File("mascotas.dat");
 
-        if(fileToDelete.exists() || fileToDelete.isFile()){
-            System.out.printf("Borrando archivo %s en directorio %s...%n", fileToDelete.getName(), fileToDelete.getPath());
+        if (fileToDelete.exists() || fileToDelete.isFile()) {
+            System.out.printf("Borrando archivo %s en directorio %s...%n", fileToDelete.getName(),
+                    fileToDelete.getPath());
+
             if (fileToDelete.delete())
                 System.out.printf("Archivo %s borrado", fileToDelete.getName());
             else
-                System.out.println("No se pudo eliminar el archivo");             
+                System.out.println("No se pudo eliminar el archivo");
         } else {
             System.out.println("No existe el archivo");
         }
