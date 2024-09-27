1c628fcb920982a7156c752baaa1702a224ff8a9
rpallin0705 <rpalomarelinares@gmail.com>
2024-09-27 11:57:25 +0100

    ACDT regularExpresion :fixed

--- ACDT/Tema1/holamundo/src/main/java/com/iesvdc/acceso/App.java
+++ ACDT/Tema1/holamundo/src/main/java/com/iesvdc/acceso/App.java
@@ -7,12 +7,13 @@ import com.iesvdc.acceso.model.Persona;
  */
 public class App {
     public static void main(String[] args) {
-        String caracteresNoPermitidos = "[á-ü]";
+        String caracteresNoPermitidos = ".*[á-ü].*";
         Persona persona = new Persona();
         persona.setNombre(" ÁLVARO");
         persona.setApellidos("Palomares López");
         String email = (persona.getNombre().trim().charAt(0) + persona.getApellidos().trim()).replace(" ", "").toLowerCase();
         System.out.println(email);
         System.out.println(email.matches(caracteresNoPermitidos));
+        
     }
 }
