47ecb13e61e57789b668e426c32d3f4612c9fb37
Rafael A Palomares Linares <rpalomareslinares@gmail.com>
2024-10-28 12:36:58 +0100

    node

---
+++ ACDT/Tema_2/gestionAcademica/Readme.md
@@ -0,0 +1,50 @@
+# Gestión académica
+
+Inicializamos la BBDD y el contenedor Docker
+
+```bash
+npm install --save express express-session mysql2 pub body-parser
+```
+
+* **express**: Servidor web para NodeJs
+* **express-session**: gestiona sesiones HTTP entre cliente y servidor
+* **mysql2**: driver para conectar a mysql
+* **pub**: motor HTML
+* **body-parser**: para convertir los datos de un formulario (POST y GET) a JSON
+  
+Inicicializamos el repositorio
+
+```bash
+git init
+git add .
+git commit -m "Primer commit"
+```
+
+Constantes para el servidor node
+
+```JavaScript
+const express = require('express');
+const session = require('express-session');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 8000;
+```
+
+Iniciar servidor
+
+```JavaScript
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+})
+```
+
+Comprobación
+
+```JavaScript
+app.get('/hola', (req, res) =>{
+    res.send('Hola mundo');
+})
+```
--- ACDT/Tema_2/gestionAcademica/app.js
+++ ACDT/Tema_2/gestionAcademica/app.js
@@ -2,21 +2,46 @@
  * Aplicación Node.js Express
  * Gestión academica
  */
-const express = require('express');
-const session = require('express-session');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = 8000;
 
+// Indica a express que el motor de vistas es pug
+app.set("view engine", "pug");
+/*
+ * Indica en que carpeta se guardan las vistas, si es "views" no hace falta esta linea
+ * pero si es otra carpeta si haría falta
+ */
+app.set("views", path.join(__dirname, "views"));
+
 // Iniciamos la aplicación en el puerto designado
 app.listen(port, () => {
-    console.log(`Servidor iniciado en http://localhost:${port}`);
-})
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
 
 // req y res son dos parametros preestablecidos que corresponden a request y response
-app.get('/hola', (req, res) =>{
-    res.send('Hola mundo');
-})
+app.get("/", (req, res) => {
+  //res.send("Hola mundo");
+
+  // Renderiza el archivo test.pug de la carpeta views o la indicada anteriormente
+  res.render("test");
+});
+
+app.get("/hola", (req, res) => {
+  const { nombre, apellido } = req.query;
+  res.send(`Hola ${nombre} ${apellido}`);
+});
+
+app.post("/hola", (req, res) => {
+  res.send(`Hola desde un post`);
+});
+
+app.post("/destino", (req, res) => {
+    console.log(req);
+    res.render("saluda", req.body);;
+});
