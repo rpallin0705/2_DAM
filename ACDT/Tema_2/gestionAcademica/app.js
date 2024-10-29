6c40b10464e3f8db1050893553693e78be404441
Rafael A Palomares Linares <rpalomareslinares@gmail.com>
2024-10-29 12:04:53 +0100

    acdt :nodejs

--- ACDT/Tema_2/gestionAcademica/app.js
+++ ACDT/Tema_2/gestionAcademica/app.js
@@ -18,30 +18,41 @@ app.set("view engine", "pug");
  * pero si es otra carpeta si haría falta
  */
 app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Iniciamos la aplicación en el puerto designado
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: 33307,
+  user: 'root',
+  password: 'zx76wbz7FG89k',
+  database: 'gestion',
+});
+
+// Conexión a MySQL
+db.connect(err => {
+  if (err) {
+    console.error('Error al conectar a MySQL:', err);
+    return;
+  }
+  console.log('Conexión exitosa a MySQL');
+});
+
 // req y res son dos parametros preestablecidos que corresponden a request y response
 app.get("/", (req, res) => {
-  //res.send("Hola mundo");
-
-  // Renderiza el archivo test.pug de la carpeta views o la indicada anteriormente
-  res.render("test");
+  res.render("index")
 });
 
-app.get("/hola", (req, res) => {
-  const { nombre, apellido } = req.query;
-  res.send(`Hola ${nombre} ${apellido}`);
-});
-
-app.post("/hola", (req, res) => {
-  res.send(`Hola desde un post`);
-});
-
-app.post("/destino", (req, res) => {
-    console.log(req);
-    res.render("saluda", req.body);;
+app.get("/alumnos", () => {
+  db.query(
+    'Select * from `alumnos`',
+    (err, result) => {
+      if (err) res.send("Error al hacer la consulta")
+      else res.render('alumnos', {alumnos: result})
+    }
+  )
 });
